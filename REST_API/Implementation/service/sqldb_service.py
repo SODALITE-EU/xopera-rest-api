a8e14725ee0474bf6b9cbebe776ed2a08fb23120
mihael_trajbaric <mihael.trajbaric@xlab.si>
2020-04-10 14:48:51 +0200

    add gitCsarDB logging
    This commit adds logging of every transaction (update, delete) to gitCsarDb. Logs are saved to SQL database of choice (PostgreSQL or OfflineStorage)
    For now, it saves folowing data, (but could be extended in future):
    - blueprint_token: token of blueprint
    - version_tag: version_tag of blueprint version
    - revision_msg: message, submitted with revision / deletion
    - git_backend: git backend, used during transaction (for instance "GitlabConnector, url: https://gitlab.com, auth_token: ****"
    - job: update or delete
    - repo_url: url to repository with blueprint
    - timestamp: timestamp of revision

--- REST_API/Implementation/service/sqldb_service.py
+++ REST_API/Implementation/service/sqldb_service.py
@@ -1,7 +1,9 @@
 import glob
+import json
 import logging as log
 import os
 import shutil
+import uuid
 
 import psycopg2
 
@@ -35,6 +37,26 @@ class Database:
         """
         pass
 
+    def save_git_transaction_data(self, blueprint_token: uuid, version_tag: str, revision_msg: str, job: str,
+                                  git_backend: str, repo_url: str):
+        """
+        Saves transaction data to database
+        """
+        pass
+
+    def get_git_transaction_data(self, blueprint_token: uuid, version_tag: str = None, all: bool = False):
+        """
+        Gets last git transaction data (if version_tag is not None, specific transaction data). If all, it returns all
+        git transaction data, that satisfy conditions
+        """
+        pass
+
+    def get_version_tags(self, blueprint_token: uuid):
+        """
+        returns list of all version tags for blueprint
+        """
+        pass
+
 
 class OfflineStorage(Database):
     def __init__(self):
@@ -43,8 +65,10 @@ class OfflineStorage(Database):
 
         if not Settings.offline_storage.exists():
             Settings.offline_storage.mkdir()
-        if not Settings.offline_log.exists():
-            Settings.offline_log.exists()
+        if not Settings.offline_deployment_log.exists():
+            Settings.offline_deployment_log.mkdir()
+        if not Settings.offline_git_log.exists():
+            Settings.offline_git_log.mkdir()
 
     @staticmethod
     def file_write(path, content, name=None):
@@ -71,7 +95,7 @@ class OfflineStorage(Database):
         """
         updates deployment log with blueprint_token, timestamp, session_token, _log
         """
-        location = "{}/{}".format(Settings.offline_log, session_token)
+        location = "{}/{}".format(Settings.offline_deployment_log, session_token)
         if os.path.exists(location):
             shutil.rmtree(location)
         os.makedirs(location)
@@ -87,7 +111,7 @@ class OfflineStorage(Database):
         In case of no results returns [0, "not enough parameters"]
         """
         if blueprint_token is not None and session_token is not None:
-            path = "{}/{}".format(Settings.offline_log, session_token)
+            path = "{}/{}".format(Settings.offline_deployment_log, session_token)
             try:
                 if not self.file_read(path, 'blueprint_token') == str(blueprint_token):
                     # combination of blueprint_token and session_token does not exist
@@ -98,7 +122,7 @@ class OfflineStorage(Database):
             return [[timestamp_util.str_to_datetime(self.file_read(path, "timestamp")), self.file_read(path, "_log")]]
 
         elif blueprint_token is not None:
-            pattern = "{}/*/blueprint_token".format(Settings.offline_log)
+            pattern = "{}/*/blueprint_token".format(Settings.offline_deployment_log)
             token_file_paths = ["/".join(path.split('/')[:-1]) for path in glob.glob(pattern) if
                                 self.file_read(path) == str(blueprint_token)]
             return [[timestamp_util.str_to_datetime(self.file_read(path, "timestamp")), self.file_read(path, "_log")]
@@ -106,7 +130,7 @@ class OfflineStorage(Database):
                     in token_file_paths]
 
         elif session_token is not None:
-            path = "{}/{}".format(Settings.offline_log, session_token)
+            path = "{}/{}".format(Settings.offline_deployment_log, session_token)
             try:
                 return [
                     [timestamp_util.str_to_datetime(self.file_read(path, "timestamp")), self.file_read(path, "_log")]]
@@ -115,6 +139,59 @@ class OfflineStorage(Database):
                 return []
         else:
             return []
+
+    def save_git_transaction_data(self, blueprint_token: uuid, version_tag: str,
+                                  revision_msg: str, job: str, git_backend: str, repo_url: str):
+        """
+        Saves transaction data to database
+        """
+        try:
+            timestamp = timestamp_util.datetime_now_to_string()
+            location = Settings.offline_git_log / str(blueprint_token)
+            if not location.exists():
+                os.makedirs(location)
+
+            git_transaction_data = {
+                'blueprint_token': str(blueprint_token),
+                'version_tag': version_tag,
+                'revision_msg': revision_msg,
+                'job': job,
+                'git_backend': git_backend,
+                'repo_url': repo_url,
+                'timestamp': timestamp
+            }
+            (location / str(timestamp)).write_text(json.dumps(git_transaction_data, indent=2))
+        except Exception as e:
+
+            log.error(f'Failed to update git log in OfflineStorage database: {str(e)}')
+            return False
+        log.info('Updated git log in OfflineStorage database')
+        return True
+
+    def get_git_transaction_data(self, blueprint_token, version_tag=None, all=False):
+        """
+        Gets last git transaction data (if version_tag is not None, specific transaction data)
+        """
+        location = Settings.offline_git_log / str(blueprint_token)
+        logfile_paths = sorted([data for data in location.glob("*")], reverse=True)  # first element was last added
+        json_logs = [json.load(file.open('r')) for file in logfile_paths]
+        if version_tag:
+            json_logs = [json_log for json_log in json_logs if json_log['version_tag'] == version_tag]
+        if all:
+            return json_logs
+        try:
+            return [json_logs[0]]
+        except IndexError:
+            return []
+
+    def get_version_tags(self, blueprint_token: uuid):
+        """
+        returns list of all version tags for blueprint
+        """
+        log_data = self.get_git_transaction_data(blueprint_token, all=True)
+        all_tags = {json_log['version_tag'] for json_log in log_data}
+        deleted_tags = {json_log['version_tag'] for json_log in log_data if json_log['job'] == 'delete'}
+        return sorted(list(all_tags - deleted_tags))
 
 
 class PostgreSQL(Database):
@@ -129,7 +206,19 @@ class PostgreSQL(Database):
                         session_token text, 
                         _log text,
                         primary key (session_token)
-                        );""".format(Settings.log_table))
+                        );""".format(Settings.deployment_log_table))
+
+        self.execute("""
+                        create table if not exists {} (
+                        blueprint_token varchar (36),
+                        version_tag varchar(36),
+                        timestamp timestamp default current_timestamp, 
+                        revision_msg text,
+                        job varchar(36), 
+                        git_backend text,
+                        repo_url text,
+                        primary key (timestamp)
+                        );""".format(Settings.git_log_table))
 
     def disconnect(self):
         log.info('disconnecting PostgreSQL database')
@@ -142,19 +231,25 @@ class PostgreSQL(Database):
                 dbcur.execute(command, replacements)
             else:
                 dbcur.execute(command)
-        except psycopg2.Error:
+        except psycopg2.Error as e:
+            log.debug(str(e))
             dbcur.execute("ROLLBACK")
+            return False
         dbcur.close()
         self.connection.commit()
-        return dbcur
+        return True
 
     def update_deployment_log(self, blueprint_token: str, _log: str, session_token: str, timestamp: str):
 
-        self.execute(
-            "insert into {} (blueprint_token, timestamp, session_token, _log) values (%s, %s, %s, %s, %s)"
-            .format(Settings.log_table), (str(blueprint_token), str(timestamp), str(session_token), str(_log)))
-
-        log.info('Updated deployment log in PostgreSQL database')
+        response = self.execute(
+            "insert into {} (blueprint_token, timestamp, session_token, _log) values (%s, %s, %s, %s)"
+                .format(Settings.deployment_log_table),
+            (str(blueprint_token), str(timestamp), str(session_token), str(_log)))
+        if response:
+            log.info('Updated deployment log in PostgreSQL database')
+        else:
+            log.error('Failed to update deployment log in PostgreSQL database')
+        return response
 
     def get_deployment_log(self, blueprint_token: str = None, session_token: str = None):
         """
@@ -168,14 +263,14 @@ class PostgreSQL(Database):
 
         if blueprint_token is not None and session_token is not None:
             query = "select timestamp, _log from {} where blueprint_token = '{}' and session_token = '{}' order by " \
-                    "timestamp desc;".format(Settings.log_table, blueprint_token, session_token)
+                    "timestamp desc;".format(Settings.deployment_log_table, blueprint_token, session_token)
 
         elif blueprint_token is not None:
             query = "select timestamp, _log from {} where blueprint_token = '{}' order by timestamp desc;".format(
-                Settings.log_table,
+                Settings.deployment_log_table,
                 str(blueprint_token))
         elif session_token is not None:
-            query = "select timestamp, _log from {} where session_token = '{}';".format(Settings.log_table,
+            query = "select timestamp, _log from {} where session_token = '{}';".format(Settings.deployment_log_table,
                                                                                         str(session_token))
         else:
             return []
@@ -185,3 +280,54 @@ class PostgreSQL(Database):
         dbcur.close()
 
         return lines
+
+    def save_git_transaction_data(self, blueprint_token: uuid, version_tag: str, revision_msg: str, job: str,
+                                  git_backend: str, repo_url: str):
+        """
+        Saves transaction data to database
+        """
+        response = self.execute(
+            """insert into {} (blueprint_token, version_tag, revision_msg, job, git_backend, repo_url) values 
+            (%s, %s, %s, %s, %s, %s)""".format(Settings.git_log_table),
+            (str(blueprint_token), version_tag, revision_msg, job, git_backend, repo_url))
+        if response:
+            log.info('Updated git log in PostgreSQL database')
+        else:
+            log.error('Fail to update git log in PostgreSQL database')
+        return response
+
+    def get_git_transaction_data(self, blueprint_token: uuid, version_tag: str = None, all: bool = False):
+        """
+        Gets all transaction data for some blueprint
+        """
+        inputs = tuple(xi for xi in (str(blueprint_token), version_tag) if xi is not None)
+        dbcur = self.connection.cursor()
+        query = """select blueprint_token, version_tag, revision_msg, job, git_backend, repo_url, timestamp
+         from {} where blueprint_token = %s {} order by timestamp desc {};""".format(
+            Settings.git_log_table, "and version_tag = %s" if version_tag else "", "limit 1" if not all else "")
+
+        dbcur.execute(query, inputs)
+        lines = dbcur.fetchall()
+        git_transaction_data_list = [
+            {
+                'blueprint_token': line[0],
+                'version_tag': line[1],
+                'revision_msg': line[2],
+                'job': line[3],
+                'git_backend': line[4],
+                'repo_url': line[5],
+                'timestamp': timestamp_util.datetime_to_str(line[6])
+            } for line in lines
+        ]
+        dbcur.close()
+
+        return git_transaction_data_list
+
+    def get_version_tags(self, blueprint_token: uuid):
+        """
+        returns list of all version tags for blueprint
+        """
+        log_data = self.get_git_transaction_data(blueprint_token, all=True)
+        all_tags = {json_log['version_tag'] for json_log in log_data}
+        deleted_tags = {json_log['version_tag'] for json_log in log_data if json_log['job'] == 'delete'}
+        return sorted(list(all_tags - deleted_tags))
