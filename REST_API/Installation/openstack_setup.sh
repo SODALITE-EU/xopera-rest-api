f1895183eae5064b9fc9c1898bfb7c667ededfc8
Kamil Tokmakov <tokmakovkamil@gmail.com>
2020-06-25 15:43:23 +0200

    fix copy of rc files openstack_setup.sh

--- REST_API/Installation/openstack_setup.sh
+++ REST_API/Installation/openstack_setup.sh
@@ -12,11 +12,11 @@ fi
 echo "OpenStack RC file location: ${rc_file_location}"
 
 echo "Copying OpenStack RC file to settings..."
-cp "$file" "Implementation/settings/openrc.sh" || exit
+cp "$file" "../Implementation/settings/openrc.sh" || exit
 echo "done"
 
 
-cd Implementation || exit
+cd ../Implementation || exit
 rm -rf settings/vault.yml || true
 
 read -s -r -p "Enter OpenStack RC password:" os_password
