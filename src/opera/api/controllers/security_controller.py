2c4e6588597e681f97623645a9b8e6f8be61305a
Alexander Maslennikov <amaslenn@gmail.com>
2021-02-19 15:03:45 +0100

    Security controller fix for introspection errors

--- src/opera/api/controllers/security_controller.py
+++ src/opera/api/controllers/security_controller.py
@@ -1,9 +1,10 @@
 import functools
 import re
-from base64 import b64encode
-
 import connexion
 import requests
+import logging
+
+from base64 import b64encode
 
 from opera.api.cli import CSAR_db, SQL_database
 from opera.api.settings import Settings
@@ -30,6 +31,9 @@ def token_info(access_token) -> dict:
     request = {'token': access_token}
     headers = {'Content-type': 'application/x-www-form-urlencoded'}
     token_info_url = Settings.oidc_introspection_endpoint_uri
+    if not token_info_url:
+        logging.warn("OAuth 2.0 Introspection endpoint not configured.")
+        return None
     # TODO add multiple client support
     basic_auth_string = '{0}:{1}'.format(
         Settings.oidc_client_id,
@@ -39,14 +43,17 @@ def token_info(access_token) -> dict:
     headers['Authorization'] = 'Basic {0}'.format(
         b64encode(basic_auth_bytes).decode('utf-8')
     )
-
-    token_request = session.post(token_info_url, data=request, headers=headers)
-    if not token_request.ok:
-        return None
-    json = token_request.json()
-    if "active" in json and json["active"] is False:
-        return None
-    return json
+    try:
+        token_request = session.post(token_info_url, data=request, headers=headers)
+        if not token_request.ok:
+            return None
+        json = token_request.json()
+        if "active" in json and json["active"] is False:
+            return None
+        return json
+    except Exception as e:
+        logging.error(str(e))
+        return None            
 
 
 def validate_scope(required_scopes, token_scopes) -> bool:
