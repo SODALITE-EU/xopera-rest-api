9b9a3ee618f082018541e71a2083b4cb02e753b6
Alexander Maslennikov <amaslenn@gmail.com>
2021-02-22 16:23:07 +0100

    Security unit test fix

--- src/opera/api/tests/test_16_security.py
+++ src/opera/api/tests/test_16_security.py
@@ -1,4 +1,5 @@
 from opera.api.controllers import security_controller
+from opera.api.settings import Settings
 
 
 class TestSecurity:
@@ -10,6 +11,10 @@ class TestSecurity:
     def test_token_info(self, mocker):
         mock = mocker.MagicMock()
         mock.ok = False
+        Settings.oidc_introspection_endpoint_uri = ""
+        result = security_controller.token_info("ACCESS_TOKEN")
+        assert result is None
+        Settings.oidc_introspection_endpoint_uri = "test"
         mocker.patch("opera.api.controllers.security_controller.session.post", return_value=mock)
         result = security_controller.token_info("ACCESS_TOKEN")
         assert result is None
